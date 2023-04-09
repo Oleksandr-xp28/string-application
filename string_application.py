53da0857b6f9d7e26f0a333297ebe4468ae1a772
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-04-09 19:10:14 +0300

    ex_2 task done

--- string_application.py
+++ string_application.py
@@ -1,1 +1,27 @@
+# feature/hw/ex_2
+# HW-2: Fedor_xp28@student.itstep.org
+
+try:
+    string = input("Enter a string: ")
+    letters = 0
+    digits = 0
+
+    for char in string:
+        if char.isalpha():
+            letters += 1
+        elif char.isdigit():
+            digits += 1
+
+    print("Number of letters:", letters)
+    print("Number of digits:", digits)
+
+except Exception as e :
+    print("Error: ", e)
+finally:
+    print("Program terminated")
+    exit()
+
+# Path: string_application.py
+
+
 
