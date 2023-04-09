87112e2db1b27ac811db036f3f81c8daf9b6c5f5
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-04-09 19:14:13 +0300

    task update

--- string_application.py
+++ string_application.py
@@ -1,3 +1,6 @@
+# feature/hw/ex_3
+# HW-2: Fedor_xp28@student.itstep.org
+
 string = input("Enter a string: ")
 char = input("Enter a character to search for: ")
 count = 0
@@ -6,5 +9,5 @@ for c in string:
     if c == char:
         count += 1
 
-print(f"The character '{char}' appears {count}.")
+print(f"Character '{char}' Appears {count}.")
 
