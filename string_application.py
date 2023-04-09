41be3b404eb4e8315a037f1521f36b490b7fccf2
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-04-09 19:05:28 +0300

    ex_ 1 task done Fedor_xp28

--- string_application.py
+++ string_application.py
@@ -1,1 +1,15 @@
+# feature/hw/ex_1
+# HW-1: Write a program that asks the user for a string and prints out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
+try:
+    text = input("Enter a string: ")
+    result = ""
+    for i in range(len(text) -1,-1,-1):
+        result += text[i]
+    print(result)
+except Exception as e :
+    print("Error: ", e)
+finally:
+#    print("Program terminated")
+    exit()
 
+# Path: string_application.py
