2f7e9335851f72322e4d51d93cb540625fe20d44
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-04-09 19:17:43 +0300

    save task

--- string_application.py
+++ string_application.py
@@ -1,13 +1,20 @@
 # feature/hw/ex_4
 # HW-2: Fedor_xp28@student.itstep.org
+try:
+    string = input("Enter a string: ")
+    word = input("Enter a word to search for: ")
+    count = 0
 
-string = input("Enter a string: ")
-word = input("Enter a word to search for: ")
-count = 0
+    words = string.split()
+    for w in words:
+        if w == word:
+            count += 1
 
-words = string.split()
-for w in words:
-    if w == word:
-        count += 1
+    print(f"Word '{word}' Appears {count}.")
+except Exception as e :
+    print("Error: ", e)
+finally:
+    print("Program terminated")
+    exit()
 
-print(f"Word '{word}' Appears {count}.")
+# Path: string_application.py
