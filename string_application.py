98b415525819701c1139d4050f0f707a4adbefd6
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-04-09 19:21:43 +0300

    ex_5 task done

--- string_application.py
+++ string_application.py
@@ -1,1 +1,19 @@
+try:
+    string = input("Enter a string: ")
+    search_word = input("Search for: ")
+    replace_word = input("Replace: ")
 
+    words = string.split()
+    for i in range(len(words)):
+        if words[i] == search_word:
+            words[i] = replace_word
+
+    new_string = " ".join(words)
+    print(new_string)
+except Exception as e :
+    print("Error: ", e)
+finally:
+#    print("Program terminated")
+    exit()
+
+# Path: string_application.py
