78a6ae19a0440ded08fb6a78c1377c3b898e6733
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-04-09 19:17:19 +0300

    ex_4 task done

--- string_application.py
+++ string_application.py
@@ -1,1 +1,13 @@
+# feature/hw/ex_4
+# HW-2: Fedor_xp28@student.itstep.org
 
+string = input("Enter a string: ")
+word = input("Enter a word to search for: ")
+count = 0
+
+words = string.split()
+for w in words:
+    if w == word:
+        count += 1
+
+print(f"Word '{word}' Appears {count}.")
