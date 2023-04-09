75fcd2b65ce4ec6b1992ad6dd05dcb32f87e3407
Fedor_xp28@student.itstep.org <Fedor_xp28@student.itstep.org>
2023-04-09 19:18:24 +0300

    update

--- string_application.py
+++ string_application.py
@@ -1,13 +1,19 @@
 # feature/hw/ex_3
 # HW-2: Fedor_xp28@student.itstep.org
+try:
+    string = input("Enter a string: ")
+    char = input("Enter a character to search for: ")
+    count = 0
 
-string = input("Enter a string: ")
-char = input("Enter a character to search for: ")
-count = 0
+    for c in string:
+        if c == char:
+            count += 1
 
-for c in string:
-    if c == char:
-        count += 1
+    print(f"Character '{char}' Appears {count}.")
+except Exception as e :
+    print("Error: ", e)
+finally:
+#    print("Program terminated")
+    exit()
 
-print(f"Character '{char}' Appears {count}.")
-
+# Path: string_application.py
